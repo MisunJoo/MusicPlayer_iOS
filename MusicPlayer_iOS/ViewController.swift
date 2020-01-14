886100ec19cef32d9efab307cdc08b53491f833a
MisunJoo <0987someday@naver.com>
2020-01-14 21:07:08 +0900

    Initial Commit

---
+++ MusicPlayer_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer_iOS
+//
+//  Created by Misun Joo on 14/01/2020.
+//  Copyright © 2020 Misun Joo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
