6b0c7ecb036af90a28da29f02e8a04d7ba6526c2
MisunJoo <0987someday@naver.com>
2020-01-14 21:25:01 +0900

    [ADD] Make Slider, Button, Label

--- MusicPlayer_iOS/ViewController.swift
+++ MusicPlayer_iOS/ViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func touchUpPlayPauseButton(_sender: UIButton){
+        print("button tapped")
+    }
+    
+    @IBAction func sliderValueChangedOne(_sender:UISlider){
+        print("slider value changed")
+    }
+    
+    
+    
+
 
 
 }
